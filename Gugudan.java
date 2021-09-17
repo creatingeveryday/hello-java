71bb3a5efbf67aca7fe3d27b64efab09b094e9e0
JongKuk Seo <jongkuk.developer@gmail.com>
2021-09-17 13:02:59 +0900

    0917 Gugudan upload 3 ~ 4

--- Gugudan.java
+++ Gugudan.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Gugudan {
 
@@ -19,25 +20,52 @@ public class Gugudan {
 				
 				//3단  변수를 사용했지만 상태변경이 잦은 경우 중복이 많음. 결과 값을 바꾸기 위해서 변수의 값을 재설정해줘야함. 
 				
-				int fourdan = 3;
-				int result = fourdan * 1 ;
+				int samdan = 3;
+				int result = samdan * 1 ;
 				System.out.println(result);
-				result = fourdan *2;
+				result = samdan *2;
 				System.out.println(result);
-				result = fourdan *3;
+				result = samdan *3;
 				System.out.println(result);
-				result = fourdan *4;
+				result = samdan *4;
 				System.out.println(result);
-				result = fourdan *5;
+				result = samdan *5;
 				System.out.println(result);
-				result = fourdan *6;
+				result = samdan *6;
 				System.out.println(result);
-				result = fourdan *7;
+				result = samdan *7;
 				System.out.println(result);
-				result = fourdan *8;
+				result = samdan *8;
 				System.out.println(result);
-				result = fourdan *9;
+				result = samdan *9;
 				System.out.println(result);
+				
+				//4단 기능을 더하여 사용자에게 숫자를 입력받아 구구단을 출력하는 방식으로 구현 Scanner class 사용
+			
+				System.out.println("출력할 구구단은? : ");
+				Scanner scanner = new Scanner(System.in);
+				int number = scanner.nextInt();
+				System.out.println("입력받은 단 : "+ number);
+				
+				int result2 = number * 1 ;
+				System.out.println(result2);
+				result2 = number * 2;
+				System.out.println(result2);
+				result2 = number * 3;
+				System.out.println(result2);
+				result2 = number * 4;
+				System.out.println(result2);
+				result2 = number * 5;
+				System.out.println(result2);
+				result2 = number * 6;
+				System.out.println(result2);
+				result2 = number * 7;
+				System.out.println(result2);
+				result2 = number * 8;
+				System.out.println(result2);
+				result2 = number * 9;
+				System.out.println(result2);
+				
 				
 				
 				
