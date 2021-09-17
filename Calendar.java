ec343ffe6be25fb17c9c15ba30380026bbe90b5c
JongKuk Seo <jongkuk.developer@gmail.com>
2021-09-18 00:16:31 +0900

    Update using class method

--- Calendar.java
+++ Calendar.java
@@ -1,15 +1,25 @@
-import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Calendar {
-
-	public static void main(String[] args) {
+	
+	public void printSampleCalendar() {
 		System.out.println("일  월  화  수  목 금 토");
 		System.out.println("--------------------");
 		System.out.println(" 1  2  3  4  5  6  7");
 		System.out.println(" 8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
+		
+	}
+	
+	int[] maxDaysOfMonth = {31,28,31,30,31,30,31,31,30,31,30,31};
+	// 메소드를 활용해서 만들어보기
+	public int getMaxDaysMonth(int month) {
+		return maxDaysOfMonth[month - 1];
+		
+	}
+	
+	public static void main(String[] args) {
 		
 //		//두 수의 합 구하기.( 키보드 입력을 받은 두 수)
 //		int a, b; 
@@ -27,15 +37,18 @@ public class Calendar {
 //		sc.close(); 
 		
 		// 숫자를 입력받아 해당하는 달의 최대 일수를 출력을 구현. 
-		// 월에 해당하는 날짜 배열에 입력.
-		int[] daysOfMonth = {31,28,31,30,31,30,31,31,30,31,30,31};
-		
 		System.out.println("달을 입력하세요.");
 		Scanner sc1 = new Scanner(System.in);
 		int inputMonth = sc1.nextInt();
 		
-		System.out.println(inputMonth+"월은 "+daysOfMonth[inputMonth - 1] +"일까지 있습니다.");
+		Calendar cal = new Calendar();
+		
+//		System.out.println(inputMonth+"월은 "+daysOfMonth[inputMonth - 1] +"일까지 있습니다.");       //배열 기능으로 출력
+		System.out.printf("%d월은 %d일까지 있습니다.\n",inputMonth , cal.getMaxDaysMonth(inputMonth));  //메소드 기능으로 출력 , printf 메소드 사용.
 		sc1.close();
+		
+		cal.printSampleCalendar();
 	}
 
+	
 }
