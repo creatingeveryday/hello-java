ab7bf690579f6f6e266d9c25bba97235f1ef960f
JongKuk Seo <jongkuk.developer@gmail.com>
2021-09-17 14:35:05 +0900

    Add number entry rules to gugudan

--- Gugudan.java
+++ Gugudan.java
@@ -42,11 +42,11 @@ public class Gugudan {
 				
 				//사용자에게 숫자를 입력받아 구구단을 출력하는 방식으로 구현 Scanner class 사용
 			
-				System.out.println("출력할 구구단은을 입력하세요. ");
-				Scanner scanner = new Scanner(System.in);
-				int number = scanner.nextInt();
-				System.out.println("입력받은 "+ number+"단을 출력합니다.");
-				
+//				System.out.println("출력할 구구단을 입력하세요. ");
+//				Scanner scanner = new Scanner(System.in);
+//				int number = scanner.nextInt();
+//				System.out.println("입력받은 "+ number+"단을 출력합니다.");
+//				
 //				System.out.println(number * 1);
 //				System.out.println(number * 2);
 //				System.out.println(number * 3);
@@ -59,18 +59,37 @@ public class Gugudan {
 				
 				
 				// 변수와 반복문을 활용해 구현. while / for 사용 
-				System.out.println("while 문을 사용해 구구단을 출력합니다. ");
-				int i = 1;
-				while(i < 10) {
-					System.out.println(number * i );
-					i++;
-					
-				}
+//				System.out.println("while 문을 사용해 구구단을 출력합니다. ");
+//				int i = 1;
+//				while(i < 10) {
+//					System.out.println(number * i );
+//					i++;
+//					
+//				}
+//				
+//				System.out.println("for문을 사용해 구구단을 출력합니다.");
+//				int j;
+//				for (j=1; j< 10; j++) {
+//					System.out.println(number * j );
+//					
+//				}
+//				
+				//조건문 개념을 활용하여 2~9까지의 숫자만을 입력받아 구구단 구현 
+				System.out.println("출력할 구구단을 입력하세요. ");
+				Scanner scanner = new Scanner(System.in);
+				int number = scanner.nextInt();
 				
-				System.out.println("for문을 사용해 구구단을 출력합니다.");
-				int j;
-				for (j=1; j< 10; j++) {
-					System.out.println(number * j );
+				if(2 <= number && number <= 9) {
+					System.out.println("입력받은 "+ number+"단을 출력합니다.");
+					System.out.println("for문을 사용합니다.");
+					int j;
+					for (j=1; j< 10; j++) {
+						System.out.println(number * j );
+						
+					}
+				}else {
+					System.out.println("2이상, 9이하의 값만 입력할 수 있습니다.");
+					
 					
 				}
 				
